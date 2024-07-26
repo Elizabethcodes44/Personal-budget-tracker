d34ddafc31e2e15f3e0a22b12c0255acc924f6b5
Elizabeth <lizbeepmc@gmail.com>
2024-07-26 14:35:33 +0300

    netlıfy deployment ınstallatıon

--- src/app.js
+++ src/app.js
@@ -11,44 +11,64 @@ const transactions = [
     name: "shopping",
     amount: 50000,
     date: new Date(),
-    type: "income",
+    type: "expenses",
   },
   {
     id: 3,
-    name: "giveaway",
-    amount: 50000,
+    name: "Rent",
+    amount: 100000,
     date: new Date(),
-    type: "income",
+    type: "expenses",
   },
   {
     id: 4,
-    name: "giveaway",
-    amount: 50000,
+    name: "Salary",
+    amount: 500000,
     date: new Date(),
     type: "income",
   },
   {
     id: 5,
-    name: "giveaway",
-    amount: 50000,
+    name: "Sanitaries",
+    amount: 10000,
     date: new Date(),
-    type: "income",
+    type: "expenses",
   },
+ 
 ];
 const list = document.getElementById("transactionList");
+const form = document.getElementById("transactionForm")
 const status = document.getElementById("status");
+const balance = document.getElementById("balance")
+const income = document.getElementById("income")
+const expenses = document.getElementById("expenses")
 const formatter = new Intl.NumberFormat("en-NG", {
   style: "currency",
   currency: "NGN",
   signDisplay: "always",
 });
+form.addEventListener('submit', addTransaction);
+function newTotal() {
+    const incomeTotal = transactions.filter(trx => trx.type === 'income').reduce((total, trx) => total + trx.amount, 0);
+    const expensesTotal = transactions.filter(trx => trx.type === 'expenses').reduce((total, trx) => total + trx.amount, 0);
+    const balanceTotal = incomeTotal - expensesTotal
+    balance.textContent = formatter.format(balanceTotal).substring(1);
+    
+    income.textContent = formatter.format(incomeTotal * -1);
+    
+    expenses.textContent = formatter.format(expensesTotal);
+}
+
 function renderList() {
   list.innerHTML = "";
   if (transactions.length === 0) {
     status.textContent = "No transactions.";
     return;
+  } else{
+    status.textContent = "";
   }
   transactions.forEach(({ id, name, date, amount, type }) => {
+    const sign = 'income' === type ? 1 : -1;
     const listItem = document.createElement("li");
     listItem.innerHTML = `
             <div class="name">
@@ -56,7 +76,7 @@ function renderList() {
             <p>${new Date(date).toLocaleDateString()}</p>
             </div>
             <div class= "amount">
-            <span>${formatter.format(amount)}</span></div>
+            <span>${formatter.format(amount * sign)}</span></div>
             <div class="action">
                 <button onclick="deleteTransaction(${id})">Delete</button>
             </div>`;
@@ -65,10 +85,28 @@ function renderList() {
   });
 }
 renderList();
+newTotal();
 function deleteTransaction(id) {
     
     const index = transactions.findIndex((trx)=> trx.id === id);
     transactions.splice(index, 1);
     renderList();
+    newTotal();
     
+}
+function addTransaction(e) {
+    e.preventDefault();
+    const formData = new formData(this);
+    //alert(formData.get('name'));
+    transactions.push({
+        id: transactions.length + 1,
+        name: formData.get("name"),
+        amount: parseFloat(formData.get("amount")),
+        date: new Date(formData.get("date")),
+        type: 'on' === formData.get('type') ? 'income' : "expense"
+    });
+    this.reset();
+
+    renderList();
+    newTotal();
 }
